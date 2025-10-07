74fb7099e63124b204da28c8e76a0116d5e69d4a
[joshua nduka] <[ndukajoshua6@gmail.com]>
2025-10-07 18:05:53 +0100

    j

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
+import ProfileDetails from '@/components/ProfileComponent/ProfileDetails'
 import React from 'react'
 import { Platform, SafeAreaView, StatusBar, StyleSheet,   } from 'react-native'
 
 export default function Profile() {
+
+
   return (
     <SafeAreaView style={styles.safearea}>
       <StatusBar barStyle="light-content" backgroundColor="#1A35BD" />
+      <ProfileDetails/>
       
     </SafeAreaView>
   )
